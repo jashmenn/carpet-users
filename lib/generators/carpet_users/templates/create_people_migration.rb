4309b029a45d90038dbc2b8c65c431444f0f8667
Nate Murray <nate@natemurray.com>
2010-04-01 16:21:02 -0700

    added person model

---
+++ lib/generators/carpet_users/templates/create_people_migration.rb
@@ -0,0 +1,20 @@
+class CreatePeople < ActiveRecord::Migration
+  def self.up
+    create_table :people do |t|
+      t.string :email
+      t.string :phone
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :country
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :people
+  end
+end
