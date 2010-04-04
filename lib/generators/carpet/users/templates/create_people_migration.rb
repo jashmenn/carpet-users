a89b851b1271f657446a62ab81331d74eb84dd36
Nate Murray <nate@natemurray.com>
2010-04-03 21:22:44 -0700

    updated migration

--- lib/generators/carpet/users/templates/create_people_migration.rb
+++ lib/generators/carpet/users/templates/create_people_migration.rb
@@ -1,6 +1,8 @@
 class CreatePeople < ActiveRecord::Migration
   def self.up
     create_table :people do |t|
+      t.string :first_name
+      t.string :last_name
       t.string :email
       t.string :phone
       t.string :address1
