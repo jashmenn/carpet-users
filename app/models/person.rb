7658bbd95dae4316d1df390c9f7296489930f2d1
Nate Murray <nate@natemurray.com>
2010-04-07 06:41:06 -0700

    added validations, however they dont seem to work

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,18 @@
 class Person < ActiveRecord::Base
   has_one :user
+  validates_format_of :phone,
+    :message => "must be a valid telephone number.",
+    :with => /^[\(\)0-9\- \+\.]{10,20} *[extension\.]{0,9} *[0-9]{0,5}$/i
+
+  validates_format_of :zip,
+    :message => "must be a valid zip code.",
+    :with => %r{\d{5}(-\d{4})?} 
+
+  validate :valid_email?
+
+  def valid_email?
+      TMail::Address.parse(email)
+  rescue
+      errors.add_to_base(“Must be a valid email”)
+  end
 end
