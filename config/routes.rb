0e53069d5a20d9877153bf3572e1280948292a0d
Nate Murray <nate@natemurray.com>
2010-04-07 06:40:16 -0700

    tweaked routes for new api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-ActionController::Routing::Routes.draw do |map|
+# ActionController::Routing::Routes.draw do |map|
+Rails.application.routes do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
