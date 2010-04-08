74c1d774a104ea982f8d4825ec5512dfb60e1903
Nate Murray <nate@natemurray.com>
2010-04-08 07:38:11 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-# ActionController::Routing::Routes.draw do |map|
-Rails.application.routes do |map|
+ActionController::Routing::Routes.draw do |map|
+# Rails.application.routes do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
