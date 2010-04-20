99031167526d4a68ce8992b19f9b8b5158bafe29
Nate Murray <nate@natemurray.com>
2010-04-19 21:15:11 -0700

    rm'd a poor choiec of before filter

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,5 @@
 class UserSessionsController < ApplicationController
   helper "fb_connect"
-  before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
   
   def new
